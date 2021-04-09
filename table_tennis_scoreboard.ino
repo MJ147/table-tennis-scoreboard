e4696da028677452fb9814ee4e80678876d6e18c
Marek Jaczewski <jaczewski147@gmail.com>
2021-04-09 22:18:00 +0200

    add buttons and second display group

--- table_tennis_scoreboard.ino
+++ table_tennis_scoreboard.ino
@@ -1,49 +1,97 @@
-#define shiftRegister 2
-#define numbers shiftRegister * 14
-int SER=5; 
-int RCLK=6;
-int SRCLK=7; 
-int registerPin[numbers];
+#define numberOfDisplays 2
+#define digitsInDisplay 2
+#define registerOutputs digitsInDisplay * 8
+
+class Button {
+    public:
+  	int pin;
+    int buttonPushCounter = 0;   // counter for the number of button presses
+    int buttonState = 0;         // current state of the button
+    int lastButtonState = 0;     // previous state of the button
+};
+
+class Display {
+  	public: 
+  	Button button;
+	int SER;
+  	int RCLK;
+    int SRCLK;
+ 	int registerPin[registerOutputs];
+};
+
+Display display1;
 
 void setup(){
-	pinMode(SER, OUTPUT);
-	pinMode(RCLK, OUTPUT);
-	pinMode(SRCLK, OUTPUT);
+    display1.SER = 5;
+    display1.RCLK = 6;
+    display1.SRCLK = 7;
+    display1.button.pin = 2;
+	  
+  
+	pinMode(display1.button.pin, INPUT_PULLUP); // Button
+	pinMode(display1.SER, OUTPUT);
+	pinMode(display1.RCLK, OUTPUT);
+	pinMode(display1.SRCLK, OUTPUT);
 	clearRegister();
 	saveRegister();
 }
 
 void clearRegister(){
-	for(int i=0; i<numbers; i++)
-		registerPin[i]=LOW;
+	for(int i=0; i<registerOutputs; i++)
+		display1.registerPin[i]=LOW;
 }
 void saveRegister(){
-	digitalWrite(RCLK, LOW); 
-	for(int i=numbers-1; i>=0; i--){
-    	digitalWrite(SRCLK, LOW);
-    	digitalWrite(SER, registerPin[i]);
-    	digitalWrite(SRCLK, HIGH); 
+	digitalWrite(display1.RCLK, LOW); 
+  
+	for(int i=registerOutputs-1; i>=0; i--){
+    	digitalWrite(display1.SRCLK, LOW);
+    	digitalWrite(display1.SER, display1.registerPin[i]);
+    	digitalWrite(display1.SRCLK, HIGH); 
  	}
-  	digitalWrite(RCLK, HIGH); 
+  	digitalWrite(display1.RCLK, HIGH); 
 }
 
 void setPin(int pinIdx, int value){
-	registerPin[pinIdx]=value;
+	display1.registerPin[pinIdx]=value;
 }
 
 void loop(){
-	int i;
+    display1.button.buttonState = digitalRead(display1.button.pin);
+  
+    if (display1.button.buttonState != display1.button.lastButtonState) {
+        if (digitalRead(2) == HIGH) {
+            display1.button.buttonPushCounter++;
+            if (display1.button.buttonPushCounter > 11) {
+                display1.button.buttonPushCounter = 0;
+            }
+            displayDigit(0, firstDigit(display1.button.buttonPushCounter));
+            displayDigit(1, lastDigit(display1.button.buttonPushCounter));
+            saveRegister();
+        }	
+      	delay(50);
+    }
+  	display1.button.lastButtonState = display1.button.buttonState;
+}
 
-	for(i=0;i<numbers;i++){
-    	displayDigit(0, i);
-    	displayDigit(1, i);
-    	saveRegister();
-    	delay(1000);
-	}
+int firstDigit(int n) {
+  	while (n >= 10) {
+        n /= 10;   
+  	}
+  
+ 	return n;
+}
+  
+
+int lastDigit(int n) {
+  	while (n <= 10) {
+        return 0;   
+  	}
+  
+    return (n % 10);
 }
 
 void displayDigit(int x, int digit) {
-	switch (digit) {
+ 	switch (digit) {
     	case 0:
             setPin(0 + 8*x, HIGH);
             setPin(1 + 8*x, HIGH);
