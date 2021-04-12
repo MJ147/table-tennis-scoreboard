9dfe82096d24b413f3898cf7dfb4e6d851b629c5
Marek Jaczewski <jaczewski147@gmail.com>
2021-04-12 21:07:57 +0200

    fix error with inverted display

--- table_tennis_scoreboard.ino
+++ table_tennis_scoreboard.ino
@@ -1,6 +1,6 @@
 #define numberOfDisplays 2
 #define digitsInDisplay 2
-#define registerOutputs digitsInDisplay * 8
+#define registerOutputs digitsInDisplay * 9
 
 class Button {
     public:
@@ -20,57 +20,103 @@ class Display {
 };
 
 Display display1;
+Display display2;
+
+Display displays[numberOfDisplays] = {display1, display2};
 
 void setup(){
-    display1.SER = 5;
-    display1.RCLK = 6;
-    display1.SRCLK = 7;
-    display1.button.pin = 2;
-	  
+  	setupDisplay(0, 5, 6, 7, 2);
+  	setupDisplay(1, 8, 9, 10, 3);
   
-	pinMode(display1.button.pin, INPUT_PULLUP); // Button
-	pinMode(display1.SER, OUTPUT);
-	pinMode(display1.RCLK, OUTPUT);
-	pinMode(display1.SRCLK, OUTPUT);
-	clearRegister();
-	saveRegister();
+  showNumber(0, 0);
+  showNumber(1, 0);
 }
 
-void clearRegister(){
-	for(int i=0; i<registerOutputs; i++)
-		display1.registerPin[i]=LOW;
+void setupDisplay(int displayNumber, int SER, int RCLK, int SRCLK, int buttonPin) {
+	displays[displayNumber].SER = SER;
+    displays[displayNumber].RCLK = RCLK;
+    displays[displayNumber].SRCLK = SRCLK;
+    displays[displayNumber].button.pin = buttonPin;
+     
+	pinMode(displays[displayNumber].button.pin, INPUT_PULLUP); // Button
+	pinMode(displays[displayNumber].SER, OUTPUT);
+	pinMode(displays[displayNumber].RCLK, OUTPUT);
+	pinMode(displays[displayNumber].SRCLK, OUTPUT);
+	clearRegister(displayNumber);
+	saveRegister(displayNumber);
 }
-void saveRegister(){
-	digitalWrite(display1.RCLK, LOW); 
+
+void clearRegister(int displayNumber){
+ 	for(int i=0; i<registerOutputs; i++) {
+		displays[displayNumber].registerPin[i]=LOW;
+    }
+}
+
+void saveRegister(int displayNumber){
+	digitalWrite(displays[displayNumber].RCLK, LOW); 
   
 	for(int i=registerOutputs-1; i>=0; i--){
-    	digitalWrite(display1.SRCLK, LOW);
-    	digitalWrite(display1.SER, display1.registerPin[i]);
-    	digitalWrite(display1.SRCLK, HIGH); 
+    	digitalWrite(displays[displayNumber].SRCLK, LOW);
+    	digitalWrite(displays[displayNumber].SER, displays[displayNumber].registerPin[i]);
+    	digitalWrite(displays[displayNumber].SRCLK, HIGH); 
  	}
-  	digitalWrite(display1.RCLK, HIGH); 
+  	digitalWrite(displays[displayNumber].RCLK, HIGH); 
 }
 
-void setPin(int pinIdx, int value){
-	display1.registerPin[pinIdx]=value;
+void setPin(int displayNumber, int pinIdx, int value){
+	displays[displayNumber].registerPin[pinIdx]=value;
 }
 
 void loop(){
-    display1.button.buttonState = digitalRead(display1.button.pin);
+	moveCounterOnButtonPress(0);  
+ 	moveCounterOnButtonPress(1);  
+}
+
+void moveCounterOnButtonPress(int displayNumber) {
+	displays[displayNumber].button.buttonState = digitalRead(displays[displayNumber].button.pin);
   
-    if (display1.button.buttonState != display1.button.lastButtonState) {
-        if (digitalRead(2) == HIGH) {
-            display1.button.buttonPushCounter++;
-            if (display1.button.buttonPushCounter > 11) {
-                display1.button.buttonPushCounter = 0;
-            }
-            displayDigit(0, firstDigit(display1.button.buttonPushCounter));
-            displayDigit(1, lastDigit(display1.button.buttonPushCounter));
-            saveRegister();
+    if (displays[displayNumber].button.buttonState != displays[displayNumber].button.lastButtonState) {
+    	if (digitalRead(displays[displayNumber].button.pin) == HIGH) {
+            int counter = ++displays[displayNumber].button.buttonPushCounter;
+         	
+        	showNumber(displayNumber, counter);
         }	
-      	delay(50);
     }
-  	display1.button.lastButtonState = display1.button.buttonState;
+  	  displays[displayNumber].button.lastButtonState = displays[displayNumber].button.buttonState;
+
+  	
+    if(isSetEnd()) {
+      	displays[0].button.buttonPushCounter = 0;
+      	displays[1].button.buttonPushCounter = 0;
+      
+    	showNumber(0, 0);  
+  		showNumber(1, 0); 
+    };
+}
+
+void showNumber(int displayNumber, int number) {
+    if (number < 10) {
+     	displayDigit(displayNumber, 0, -1);
+      	displayDigit(displayNumber, 1, number);
+    } else {
+      	displayDigit(displayNumber, 0, firstDigit(number));
+      	displayDigit(displayNumber, 1, lastDigit(number));
+    }
+
+    saveRegister(displayNumber);
+  	delay(50);
+}
+
+boolean isSetEnd() {
+	int counter1 = displays[0].button.buttonPushCounter;
+	int counter2 = displays[1].button.buttonPushCounter;
+      
+  	if ((counter1 > 10 || counter2 > 10) && abs(counter1 - counter2) > 1) {
+      delay(3000);
+        return true;
+    }
+    
+ 	return false;
 }
 
 int firstDigit(int n) {
@@ -90,116 +136,116 @@ int lastDigit(int n) {
     return (n % 10);
 }
 
-void displayDigit(int x, int digit) {
+void displayDigit(int y, int x, int digit) {
  	switch (digit) {
     	case 0:
-            setPin(0 + 8*x, HIGH);
-            setPin(1 + 8*x, HIGH);
-            setPin(2 + 8*x, HIGH);
-            setPin(3 + 8*x, HIGH);
-            setPin(4 + 8*x, HIGH);
-            setPin(5 + 8*x, HIGH);
-            setPin(6 + 8*x, LOW);
+            setPin(y, 0 + 8*x, HIGH);
+            setPin(y, 1 + 8*x, HIGH);
+            setPin(y, 2 + 8*x, HIGH);
+            setPin(y, 3 + 8*x, HIGH);
+            setPin(y, 4 + 8*x, HIGH);
+            setPin(y, 5 + 8*x, HIGH);
+            setPin(y, 6 + 8*x, LOW);
     	break; 
     
    		case 1:
-            setPin(0 + 8*x, LOW);
-            setPin(1 + 8*x, HIGH);
-            setPin(2 + 8*x, HIGH);
-            setPin(3 + 8*x, LOW);
-            setPin(4 + 8*x, LOW);
-            setPin(5 + 8*x, LOW);
-            setPin(6 + 8*x, LOW);
+            setPin(y, 0 + 8*x, LOW);
+            setPin(y, 1 + 8*x, HIGH);
+            setPin(y, 2 + 8*x, HIGH);
+            setPin(y, 3 + 8*x, LOW);
+            setPin(y, 4 + 8*x, LOW);
+            setPin(y, 5 + 8*x, LOW);
+            setPin(y, 6 + 8*x, LOW);
    		break; 
     
         case 2:
-            setPin(0 + 8*x, HIGH);
-            setPin(1 + 8*x, HIGH);
-            setPin(2 + 8*x, LOW);
-            setPin(3 + 8*x, HIGH);
-            setPin(4 + 8*x, HIGH);
-            setPin(5 + 8*x, LOW);
-            setPin(6 + 8*x, HIGH);
+            setPin(y, 0 + 8*x, HIGH);
+            setPin(y, 1 + 8*x, HIGH);
+            setPin(y, 2 + 8*x, LOW);
+            setPin(y, 3 + 8*x, HIGH);
+            setPin(y, 4 + 8*x, HIGH);
+            setPin(y, 5 + 8*x, LOW);
+            setPin(y, 6 + 8*x, HIGH);
         break; 
 
         case 3:
-            setPin(0 + 8*x, HIGH);
-            setPin(1 + 8*x, HIGH);
-            setPin(2 + 8*x, HIGH);
-            setPin(3 + 8*x, HIGH);
-            setPin(4 + 8*x, LOW);
-            setPin(5 + 8*x, LOW);
-            setPin(6 + 8*x, HIGH);
-        break; 
+            setPin(y, 0 + 8*x, HIGH);
+            setPin(y, 1 + 8*x, HIGH);
+            setPin(y, 2 + 8*x, HIGH);
+            setPin(y, 3 + 8*x, HIGH);
+            setPin(y, 4 + 8*x, LOW);
+            setPin(y, 5 + 8*x, LOW);
+            setPin(y, 6 + 8*x, HIGH);
+        break;
 
         case 4:
-            setPin(0 + 8*x, LOW);
-            setPin(1 + 8*x, HIGH);
-            setPin(2 + 8*x, HIGH);
-            setPin(3 + 8*x, LOW);
-            setPin(4 + 8*x, LOW);
-            setPin(5 + 8*x, HIGH);
-            setPin(6 + 8*x, HIGH);
+            setPin(y, 0 + 8*x, LOW);
+            setPin(y, 1 + 8*x, HIGH);
+            setPin(y, 2 + 8*x, HIGH);
+            setPin(y, 3 + 8*x, LOW);
+            setPin(y, 4 + 8*x, LOW);
+            setPin(y, 5 + 8*x, HIGH);
+            setPin(y, 6 + 8*x, HIGH);
         break; 
 
         case 5:
-            setPin(0 + 8*x, HIGH);
-            setPin(1 + 8*x, LOW);
-            setPin(2 + 8*x, HIGH);
-            setPin(3 + 8*x, HIGH);
-            setPin(4 + 8*x, LOW);
-            setPin(5 + 8*x, HIGH);
-            setPin(6 + 8*x, HIGH);
+            setPin(y, 0 + 8*x, HIGH);
+            setPin(y, 1 + 8*x, LOW);
+            setPin(y, 2 + 8*x, HIGH);
+            setPin(y, 3 + 8*x, HIGH);
+            setPin(y, 4 + 8*x, LOW);
+            setPin(y, 5 + 8*x, HIGH);
+            setPin(y, 6 + 8*x, HIGH);
         break; 
 
         case 6:
-            setPin(0 + 8*x, HIGH);
-            setPin(1 + 8*x, LOW);
-            setPin(2 + 8*x, HIGH);
-            setPin(3 + 8*x, HIGH);
-            setPin(4 + 8*x, HIGH);
-            setPin(5 + 8*x, HIGH);
-            setPin(6 + 8*x, HIGH);
+            setPin(y, 0 + 8*x, HIGH);
+            setPin(y, 1 + 8*x, LOW);
+            setPin(y, 2 + 8*x, HIGH);
+            setPin(y, 3 + 8*x, HIGH);
+            setPin(y, 4 + 8*x, HIGH);
+            setPin(y, 5 + 8*x, HIGH);
+            setPin(y, 6 + 8*x, HIGH);
         break;
 
         case 7:
-            setPin(0 + 8*x, HIGH);
-            setPin(1 + 8*x, HIGH);
-            setPin(2 + 8*x, HIGH);
-            setPin(3 + 8*x, LOW);
-            setPin(4 + 8*x, LOW);
-            setPin(5 + 8*x, LOW);
-            setPin(6 + 8*x, LOW);
+            setPin(y, 0 + 8*x, HIGH);
+            setPin(y, 1 + 8*x, HIGH);
+            setPin(y, 2 + 8*x, HIGH);
+            setPin(y, 3 + 8*x, LOW);
+            setPin(y, 4 + 8*x, LOW);
+            setPin(y, 5 + 8*x, LOW);
+            setPin(y, 6 + 8*x, LOW);
         break;
 
         case 8:
-            setPin(0 + 8*x, HIGH);
-            setPin(1 + 8*x, HIGH);
-            setPin(2 + 8*x, HIGH);
-            setPin(3 + 8*x, HIGH);
-            setPin(4 + 8*x, HIGH);
-            setPin(5 + 8*x, HIGH);
-            setPin(6 + 8*x, HIGH);
+            setPin(y, 0 + 8*x, HIGH);
+            setPin(y, 1 + 8*x, HIGH);
+            setPin(y, 2 + 8*x, HIGH);
+            setPin(y, 3 + 8*x, HIGH);
+            setPin(y, 4 + 8*x, HIGH);
+            setPin(y, 5 + 8*x, HIGH);
+            setPin(y, 6 + 8*x, HIGH);
         break;
 
         case 9:
-            setPin(0 + 8*x, HIGH);
-            setPin(1 + 8*x, HIGH);
-            setPin(2 + 8*x, HIGH);
-            setPin(3 + 8*x, HIGH);
-            setPin(4 + 8*x, LOW);
-            setPin(5 + 8*x, HIGH);
-            setPin(6 + 8*x, HIGH);
+            setPin(y, 0 + 8*x, HIGH);
+            setPin(y, 1 + 8*x, HIGH);
+            setPin(y, 2 + 8*x, HIGH);
+            setPin(y, 3 + 8*x, HIGH);
+            setPin(y, 4 + 8*x, LOW);
+            setPin(y, 5 + 8*x, HIGH);
+            setPin(y, 6 + 8*x, HIGH);
         break;
 
         default:
-            setPin(0 + 8*x, LOW);
-            setPin(1 + 8*x, LOW);
-            setPin(2 + 8*x, LOW);
-            setPin(3 + 8*x, LOW);
-            setPin(4 + 8*x, LOW);
-            setPin(5 + 8*x, LOW);
-            setPin(6 + 8*x, LOW);
+            setPin(y, 0 + 8*x, LOW);
+            setPin(y, 1 + 8*x, LOW);
+            setPin(y, 2 + 8*x, LOW);
+            setPin(y, 3 + 8*x, LOW);
+            setPin(y, 4 + 8*x, LOW);
+            setPin(y, 5 + 8*x, LOW);
+            setPin(y, 6 + 8*x, LOW);
         break;
   }
 }
